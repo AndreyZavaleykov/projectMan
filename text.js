b90ccfb9095a562545e95963575d5feac47450ae
Андрей <93549723+AndreyZavaleykov@users.noreply.github.com>
2021-11-26 00:02:49 +0300

    Add files via upload

--- text.js
+++ text.js
@@ -19,26 +19,13 @@
 }
 
     function go() {
-    if (i_str<=mess[i].length-1) {
-    i_str++;
-    get_text();
-    if (document.all) typewriter.innerHTML = msg;
-    else if (document.layers) {
-    document.typewriter.document.write(msg);
-    document.typewriter.document.close();
-}
-    else document.getElementById("typewriter").innerHTML = msg;
-    var timer = setTimeout("go()", speed);
-}
-    else {
-    clearTimeout(timer);
-    var timer = setTimeout("changemess()", pause);
-}
-}
-    function changemess() {
-    i++;
-    i_str = 0;
-    if (i>mess.length-1) {i = 0;
-        return;}
-    go();
+        i_str++;
+        get_text();
+        if (document.all) typewriter.innerHTML=msg;
+        else if (document.layers) {
+            document.typewriter.document.write(msg);
+            document.typewriter.document.close();
+        }
+        else document.getElementById("typewriter").innerHTML=msg;
+        var timer=setTimeout("go()", speed);
 }
