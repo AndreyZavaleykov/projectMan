fb4684023fc22634c49dd0f871fa8fa3d0cd96a5
Андрей <93549723+AndreyZavaleykov@users.noreply.github.com>
2021-11-25 15:52:28 +0300

    Add files via upload

--- a.js
+++ a.js
@@ -33,7 +33,7 @@ button2.addEventListener("click", () => {
 // Копируем километраж
 button3.addEventListener("click", () => {
     let km=document.querySelector(".km").innerText;
-    let splitkm=km.split("км")
+    let splitkm=km.split(" км")
     let km1=splitkm[0].split("Протяженность маршрута: ")
     navigator.clipboard.writeText(km1[1])
 
